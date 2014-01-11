483410ebd96a8c9fc6e780bc24d33d38d9745f03
mirgags <markmiraglia@gmail.com>
2014-01-10 16:56:34 -0800

    added put and delete

--- twpm.py
+++ twpm.py
@@ -10,45 +10,70 @@ import base64
 #    def __init__(self):
 #        self.id_num = None
 #        self.url = ""
+#        self.data = None
 #        self.api = None
 
+### Methods to create for apiCall class
+#    def createTasklist(self, data):
+#    def createTask(self, data):
+#    def editTask(self, data):
+#    def closeTask(self)
+
 ### Retrieve API key from local file ./teamworkpm_api_key.txt
-def getApiKey():
-    curPath = os.getcwd()
-    f = open('%s/teamworkpm_api_key.txt' % curPath, 'rb')
-    apikey = f.readline().strip()
-    f.close()
-    return apikey
+    def getApiKey():
+        curPath = os.getcwd()
+        f = open('%s/teamworkpm_api_key.txt' % curPath, 'rb')
+        apikey = f.readline().strip()
+        f.close()
+        return apikey
 
 ### Create authorization handler for TeamworkPM
-def authUrl(theurl):
-    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passman.add_password(None, theurl, getApiKey(), 'x')
-    
-    authhandler = urllib2.HTTPBasicAuthHandler(passman)
-    
-    opener = urllib2.build_opener(authhandler)
-    
-    urllib2.install_opener(opener)
-    return
+    def authUrl(theurl):
+        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, theurl, getApiKey(), 'x')
+        
+        authhandler = urllib2.HTTPBasicAuthHandler(passman)
+        
+        opener = urllib2.build_opener(authhandler)
+        
+        urllib2.install_opener(opener)
+        return
 
 ### GET request to establish parameters
-def getUrl(theurl):
-    
-    authUrl(theurl)
-    pagehandle = urllib2.urlopen(theurl)
-    
-    return pagehandle.read()
+    def getUrl(theurl):
+        
+        authUrl(theurl)
+        pagehandle = urllib2.urlopen(theurl)
+        
+        return pagehandle.read()
 
 ### POST request accepts the Teamwork-specific URL and a JSON object with      the necessary parameters for the action.
-def postUrl(theurl, thePost):
+    def postUrl(theurl, thePost):
+    
+        req = urllib2.Request(theurl)
+        auth = 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
+        req.add_header('Authorization', auth)
+        req.add_header('Content-Type', 'application/json')
+    
+        return urllib2.urlopen(req, json.dumps(thePost))
 
-    req = urllib2.Request(theurl)
-    auth = 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
-    req.add_header('Authorization', auth)
-    req.add_header('Content-Type', 'application/json')
+### PUT request accepts the Teamwork-specific URL and a JSON object with       the necessary parameters for the action.
+    def putUrl(theurl):
+        req = urllib2.Request(url)
+        auth = 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
+        req.add_header('Authorization', auth)
+        req.add_header("Content-Type", "application/json")
+        req.get_method = lambda: "PUT"
+        return pagehandle = urllib2.urlopen(req, json.dumps(thePut))
 
-    return urllib2.urlopen(req, json.dumps(thePost))
+### DELETE request accepts the Teamwork-specific URL.
+    def deleteUrl(theurl):
+        req = urllib2.Request(url)
+        auth = 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
+        req.add_header('Authorization', auth)
+        req.add_header("Content-Type", "application/json")
+        req.get_method = lambda: "DELETE"
+        return pagehandle = urllib2.urlopen(req)
 
 ### This is test code that has been moved to the test file teamwork_test.py
 #theurl = 'http://clients.pint.com/todo_items/2592624.json'
