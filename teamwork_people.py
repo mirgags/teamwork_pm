28fa6c3a443d70a8a12544a54de804629ee3a3b6
mirgags <markmiraglia@gmail.com>
2014-02-25 09:48:49 -0800

    csv contact dump works

--- teamwork_people.py
+++ teamwork_people.py
@@ -11,7 +11,7 @@ for project in projectDict['projects']:
     if project['name'].lower().find('maintenance') > 0:
         if not any(project['company']['id'] in s for s in companyList):
             companyList.append({'companyId': project['company']['id'],                                     'companyName': project['company']['name'],                                 'contacts': []                                                           })
-            print project['company']['id']
+#            print project['company']['id']
             counter += 1
 
 counter = 0
@@ -22,39 +22,42 @@ for company in companyList:
             company['contacts'].append({person['email-address']:                                                  {'firstname': person['first-name'],                                         'lastname': person['last-name']}                                           })
             counter += 1
 
-for company in companyList:
-    print "*****"
-    print 'Id # is: ', company['companyId'], '\n'
-    print 'Name is: ', company['companyName'], '\n'
-    print 'Contacts are: ', company['contacts'], '\n'
-    print "*****"
-
-print "Count is: %s" % counter
+#for company in companyList:
+#    print "*****"
+#    print 'Id # is: ', company['companyId'], '\n'
+#    print 'Name is: ', company['companyName'], '\n'
+#    print 'Contacts are: ', company['contacts'], '\n'
+#    print "*****"
+#
+#print "Count is: %s" % counter
 
 f = open('teamwork_people_%s.csv' % datetime.datetime.now(), 'wb')
 f.write('CompanyID|CompanyName|Contacts|Emails\n')
 for company in companyList:
     name = ''
     email = ''
+    counter = 0
     for contact in company['contacts']:
-        counter = 0
-        for emailAddress in contact:
-            counter += 1
-        print contact
-        print 'counter is: ', counter
+        counter += 1
+    for contact in company['contacts']:
         for emailAddress in contact:
             if counter == 1:
                 try:
                     name = name + contact[emailAddress]['firstname']+ ' ' +                                 contact[emailAddress]['lastname']
-                    email = emailAddress
+                    email = email + '%s' % emailAddress
                 except KeyError:
                     pass
-            elif counter >=2 :
+            elif counter >= 2:
                 try:
                     name = name + contact[emailAddress]['firstname']+ ' ' +                                 contact[emailAddress]['lastname'] + ', '
                     email = email + '%s,' % emailAddress
                 except KeyError:
                     pass
             counter -= 1
+        print 'company is: ', company['companyName']
+        print 'names are: ', name
+        print 'emails are: ', email
+        print 'counter is: ', counter
     f.write('%s|%s|%s|%s\n' % (company['companyId'],                                                      company['companyName'],                                                    name, email))
+
 f.close()
