c72a03752b32cdebb8682f37d7de0dae89a9439a
mirgags <markmiraglia@gmail.com>
2014-12-11 10:53:11 -0800

    working on files

--- modTasks.py
+++ modTasks.py
@@ -2,14 +2,8 @@
 from twpm import *
 import requests
 
-if __name__ == "__main__":
-    auth = 'Basic '+base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
-    print auth
+def unPrivateTasks (headers, taskList):
     payload = {'todo-item': {'private':0}}
-    headers = {'Content-Type': 'application/json', \
-               'Authorization': auth}
-    taskList = json.loads(getUrl(\
-        'http://clients.pint.com/projects/85150/todo_lists.json'))
     for thing in taskList['todo-lists']:
         for theTask in thing['todo-items']:
             print theTask['private']
@@ -18,3 +12,27 @@ if __name__ == "__main__":
                 r = requests.put(url, data=json.dumps(payload),\
                      headers=headers)
                 print r.text 
+
+def privateFiles (auth, projectID):
+    payload = {'file': {'private':1}}
+    r = json.loads(getUrl(\
+    'http://clients.pint.com/projects/%s/files.json' % projectID))
+    for theFile in r['project']['files']:
+        print theFile['originalName']
+        if theFile['private'] == '0':
+            print 'should change to private'
+            print 'http://clients.pint.com/files/%s.json' % theFile['id']
+            p = requests.put('http://clients.pint.com/files/%s.json' % \
+                theFile['id'], data=json.dumps(payload),\
+                headers=headers)
+            print p.text
+
+if __name__ == "__main__":
+    auth = 'Basic '+base64.urlsafe_b64encode("%s:%s" % (getApiKey(), 'x'))
+    print auth
+    headers = {'Content-Type': 'application/json', \
+               'Authorization': auth}
+#    taskList = json.loads(getUrl(\
+#        'http://clients.pint.com/projects/85150/todo_lists.json'))
+#    unPrivateTasks(headers, taskList)
+    privateFiles(auth, 85151)
